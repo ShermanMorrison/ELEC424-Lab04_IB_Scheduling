ec29f33460b53a825493af79b31101b0919bff18
Jonathan Wilson <jdw7@rice.edu>
2014-10-16 02:51:04 -0500

    changed scheduler to use TIM2

--- src/scheduling.c
+++ src/scheduling.c
@@ -1,6 +1,6 @@
 #include <scheduling.h>
 
-
+int led = 0;
 
 /*
 * Enable timer and set to interrupt.
@@ -8,7 +8,7 @@
 void InitializeTimer(int tim, int pre, int per)
 {
 
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
     
     TIM_TimeBaseInitTypeDef timerInitStructure;
     timerInitStructure.TIM_Prescaler = 7200;
@@ -16,9 +16,9 @@ void InitializeTimer(int tim, int pre, int per)
     timerInitStructure.TIM_Period = 10;
     timerInitStructure.TIM_ClockDivision = TIM_CKD_DIV2;
     timerInitStructure.TIM_RepetitionCounter = 0;
-    TIM_TimeBaseInit(TIM5, &timerInitStructure); // configure timer
-    TIM_Cmd(TIM5, ENABLE); // enable the counter for timer
-    TIM_ITConfig(TIM5, TIM_IT_Update, ENABLE); // enable interrupt on timer
+    TIM_TimeBaseInit(TIM2, &timerInitStructure); // configure timer
+    TIM_Cmd(TIM2, ENABLE); // enable the counter for timer
+    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE); // enable interrupt on timer
 }
 
 /*
@@ -38,33 +38,42 @@ void InitializeInterruptGen(int TIM_IRQn)
 /*
 * Interrupt Callback for the LEDs, flips green light at 1hz
 */
-void TIM5_IRQHandler()
+void TIM2_IRQHandler()
 {
-    count += 1;
-    // If interrupt set, reset interrupt and write the toggled led state
-    if (count == 10)
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update)!= RESET)
     {
-	functions = 00001;	
+        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+
+	    count += 1;
+	    // If interrupt set, reset interrupt and write the toggled led state
+	    if (count == 10)
+	    {
+		functions = 00001;	
+	    }
+	    if (count == 100)
+	    {
+		
+		functions = 00011;
+	    }
+	    if (count == 995){
+		functions = 10000;
+	    }
+	    if (count == 1000)
+	    {
+		led = 1-led;
+		GPIO_WriteBit(GPIOB, GPIO_Pin_5, led);
+		functions = 01111;
+		count = 0;
+	    }
+
     }
-    if (count == 100)
-    {
-	functions = 00011;
-    }
-    if (count == 995){
-	functions = 10000;
-;
-    }
-    if (count == 1000)
-    {
-	functions = 01111;
-	count = 0;
-    }
+
 }
 
 void schedule()
 {
-    InitializeTimer(TIM5_IRQn, 7200,10);
-    InitializeInterruptGen(TIM5_IRQn);
+    InitializeTimer(TIM2_IRQn, 7200,10);
+    InitializeInterruptGen(TIM2_IRQn);
 }
 
 
